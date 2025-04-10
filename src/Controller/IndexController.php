e8d8ca64f5b9dc292cbd4e6232f20e515c7e3eb1
root <root@vbox.home>
2025-04-10 20:23:47 +0300

    Migrations done

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController{
+    #[Route('/', name: 'index')]
+    public function index() : JsonResponse{
+        return new JsonResponse(['text' => 'FEED BACK TO CLIENT2']);
+    }
+}
+?>
